5db24511b98a59eb7933ee3e452a99aec7844c0b
Xarv <strzelecki20@gmail.com>
2019-08-21 11:57:26 +0200

    Updates

---
+++ precache-manifest.fa6461b15be1e72771caf0bb13b59396.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c61205bfb613adb2dd2c1228b64bf5",
+    "url": "/simple-react-app/index.html"
+  },
+  {
+    "revision": "903079f02638e7fe2f9a",
+    "url": "/simple-react-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7a8819ce2dcf9968cf0e",
+    "url": "/simple-react-app/static/css/main.bc017eed.chunk.css"
+  },
+  {
+    "revision": "903079f02638e7fe2f9a",
+    "url": "/simple-react-app/static/js/2.7f766d0e.chunk.js"
+  },
+  {
+    "revision": "7a8819ce2dcf9968cf0e",
+    "url": "/simple-react-app/static/js/main.e302b1db.chunk.js"
+  },
+  {
+    "revision": "6fd64f1acbc6de00f5ea",
+    "url": "/simple-react-app/static/js/runtime~main.d5660f83.js"
+  }
+]);
